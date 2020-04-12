57333fcaa800292d24e7a98918bc437cd286aa4a
gramatikovska2 <61655206+gramatikovska2@users.noreply.github.com>
2020-04-12 20:11:00 +0200

    student class finished

--- Student.java
+++ Student.java
@@ -1,18 +1,55 @@
+import java.util.*; 
+import java.util.function.Supplier;
 class Student {
 	String index;
 	String firstName;
 	String lastName;
-	List<Integer> list = new labPoints<Integer>(); 
+	List<Integer> list = new labPoints<Integer>();
 
-	//TODO constructor
+	// TODO constructor
+	
+	public Student(String index, String firstName, String lastName) {
+		index=index;
+		firstName=firstName;
+		lastName=lastName;
+	}
 
-	//TODO seters & getters
+	// TODO seters & getters
+	public void setIndex(String index) {
+		this.index=ind;
+	}
+	public String getIndex() {
+		return this.index;
+	}
+	public void setFName(String fisrtName) {
+		this.firstName=fName;
+	}
+	public String getFName() {
+		return this.firstName;
+	}
+	public void setLName(lastName) {
+		this.lastName=lname;
+	}
+	public String getLName() {
+		return this.lastName;
+	}
 
 	public double getAverage() {
-		//TODO
+		float prosek;
+		int temp=0;
+		temp+=labPoints;
+		return prosek;
+		// TODO
 	}
 
 	public boolean hasSignature() {
-		//TODO
+		if (labPoints>8) {
+			return 1;
+		}
+		else {
+			return 0;
+		}
+		// TODO
+		
 	}
 }
